babea8901df5de44149799c6a35d709fd68f9fc3
Redwan <moody.d78190>
2024-06-15 19:58:27 +0200

    update

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -32,7 +32,7 @@ function Footer() {
             </li>
             <li className="social-icons">
               <a
-                href="https://twitter.com/Soumyajit4419"
+                href="https://twitter.com/Moody78190"
                 style={{ color: "white" }}
                 target="_blank" 
                 rel="noopener noreferrer"
@@ -42,7 +42,7 @@ function Footer() {
             </li>
             <li className="social-icons">
               <a
-                href="https://www.linkedin.com/in/soumyajit4419/"
+                href="https://www.linkedin.com/in/moody-d-94206026a"
                 style={{ color: "white" }}
                 target="_blank" 
                 rel="noopener noreferrer"
